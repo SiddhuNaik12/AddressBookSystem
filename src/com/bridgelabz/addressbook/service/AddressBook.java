7bf2340f2a1cb3948c79be2246e35c6522a32f06
SiddhuNaik12 <bsanaik005@gmail.com>
2025-12-23 15:10:34 +0530

    UC2: Add new contact using console input

--- src/com/bridgelabz/addressbook/service/AddressBook.java
+++ src/com/bridgelabz/addressbook/service/AddressBook.java
@@ -1,48 +1,47 @@
 package com.bridgelabz.addressbook.service;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 //import java.util.Iterator;
-//import java.util.Scanner;
-//
-//import com.bridgelabz.addressbook.model.Contact;
+import java.util.Scanner;
+import com.bridgelabz.addressbook.model.Contact;
 
 public class AddressBook {
-//	ArrayList<Contact> contacts = new ArrayList<>();
-//	Scanner sc = new Scanner(System.in);
-//	// UC2 - Add Contact
-//	public void addContact() {
-//
-//		System.out.print("First Name: ");
-//		String firstName = sc.next();
-//
-//		System.out.print("Last Name: ");
-//		String lastName = sc.next();
-//
-//		System.out.print("Address: ");
-//		String address = sc.next();
-//
-//		System.out.print("City: ");
-//		String city = sc.next();
-//
-//		System.out.print("State: ");
-//		String state = sc.next();
-//
-//		System.out.print("Zip: ");
-//		String zip = sc.next();
-//
-//		System.out.print("Phone: ");
-//		String phone = sc.next();
-//
-//		System.out.print("Email: ");
-//		String email = sc.next();
-//
-//		Contact contact = new Contact(
-//				firstName, lastName, address,
-//				city, state, zip, phone, email);
-//
-//		contacts.add(contact);
-//		System.out.println("Contact Added Successfully");
-//	}
+	ArrayList<Contact> contacts = new ArrayList<>();
+	Scanner sc = new Scanner(System.in);
+	// UC2 - Add Contact
+	public void addContact() {
+
+		System.out.print("First Name: ");
+		String firstName = sc.next();
+
+		System.out.print("Last Name: ");
+		String lastName = sc.next();
+
+		System.out.print("Address: ");
+		String address = sc.next();
+
+		System.out.print("City: ");
+		String city = sc.next();
+
+		System.out.print("State: ");
+		String state = sc.next();
+
+		System.out.print("Zip: ");
+		String zip = sc.next();
+
+		System.out.print("Phone: ");
+		String phone = sc.next();
+
+		System.out.print("Email: ");
+		String email = sc.next();
+
+		Contact contact = new Contact(
+				firstName, lastName, address,
+				city, state, zip, phone, email);
+
+		contacts.add(contact);
+		System.out.println("Contact Added Successfully");
+	}
 //	// UC3 - Edit Contact
 //	public void editContact() {
 //
